37878a01607a17bd22c5056ac7c6dab280922de6
Vladimir Ivanov <luvolunov@gmail.com>
2022-12-02 14:08:11 +0900

    feat: added test file

---
+++ index.ts
@@ -0,0 +1,18 @@
+import { Application } from 'https://deno.land/x/oak/mod.ts';
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = `
+    <!DOCTYPE html>
+    <html>
+      <head><title>Hello oak!</title><head>
+      <body>
+        <h1>Привет всем на кальянной!!!</h1>
+        <h2>Почему не работаем? :)</h2>
+      </body>
+    </html>
+  `;
+});
+
+await app.listen({ port: 8000 });
